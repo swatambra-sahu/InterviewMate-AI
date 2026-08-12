bf375f36fc8646e9b40ad0effc8ae3c36b543c54
Swatambra Sahu <swatambrasahu@gmail.com>
2026-08-12 16:46:26 +0530

    Update CORS origin to production URL

--- server/index.js
+++ server/index.js
@@ -11,7 +11,8 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    // origin:"http://localhost:5173",
+    origin:"https://interviewmate-ai-3un7.onrender.com",
     credentials:true
 }))
 
